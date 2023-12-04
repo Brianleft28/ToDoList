7cb4797f8bef3051ecaab3a9a5acb1c803220ee1
Brianleft28 <contactobrianleft@gmail.com>
2023-12-04 12:42:01 -0300

    toDoList 0.2

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-// declaro
-const btnADDitem = document.querySelector('.btn')
+const divSinTareas = document.getElementById('divTareasAdd')
+divSinTareas.style.display = 'none';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // BTN AGREGAR TAREA //
+    const btnAddItem = document.getElementById('btnAddItem');
+    btnAddItem.addEventListener('click', function() {
+      var divTareas = document.getElementById('DivTareas');
+      divTareas.style.display = 'none';
+    });
+
+  });
