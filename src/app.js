acf59a391e27d02db42c7c9b1be3325f33fc7d6d
Brianleft28 <contactobrianleft@gmail.com>
2023-12-05 13:52:01 -0300

    13:51

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+const taskInput = document.getElementById("taskInput");
+const btnAddTask = document.getElementById("btnAddTask");
+const taskList = document.getElementById("taskList");
 
+function AñadirTarea() {
+  const tareaNueva = taskInput.value;
+  if (tareaNueva.trim() !== '') {
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = tareaNueva;
+
+    taskList.appendChild(nuevaTarea);
+
+    taskInput.value = "";
+  } else {
+    alert("Por favor, ingrese una tarea válida.");
+  }
+}
+
+btnAddTask.addEventListener("click", (e) => {
+  e.preventDefault;
+  AñadirTarea();
+  taskList.style.display = 'block';
+});
